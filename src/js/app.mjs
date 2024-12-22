4a88981f73492eeaaae94ed736557d7d816f2127
Francisco Carusso <francisconicolascarusso@gmail.com>
2024-12-22 02:32:28 -0300

    feat(API): :zap: Consumición de API para los productos con fech

--- src/js/app.mjs
+++ src/js/app.mjs
@@ -1,5 +1,21 @@
 import { skeleton } from './modules/skeleton-img.mjs';
+import { fetchProducts } from './modules/api.mjs';
+import { createCard } from './modules/cards.mjs';
 
 document.addEventListener('DOMContentLoaded', () => {
-    skeleton();
+  skeleton();
 });
+
+const $CONTAINER_CARDS = document.querySelector('#container_cards');
+
+fetchProducts()
+  .then(json => {
+    json.forEach(producto => {
+      const cardHTML = createCard(producto);
+      $CONTAINER_CARDS.innerHTML += cardHTML;
+    });
+  })
+  .catch(err => {
+    console.error('Error al obtener los productos:', err);
+    $CONTAINER_CARDS.innerHTML = '<p>Hubo un problema al cargar los productos. Por favor, inténtalo más tarde.</p>';
+  });
